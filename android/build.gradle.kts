570b026d2c4a3090b15bd1067500dfc8a15ed15f
Aditya Verma <adityaverma15.cs@gmail.com>
2026-03-10 13:35:20 +0530

    fix gradle evaluation error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,40 +1,22 @@
-import com.android.build.gradle.LibraryExtension
-
 allprojects {
-repositories {
-google()
-mavenCentral()
-}
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-val newSubprojectBuildDir = newBuildDir.dir(project.name)
-project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
 subprojects {
-project.evaluationDependsOn(":app")
-}
-
-/*
-FIX: Namespace error for plugins like call_log
-*/
-subprojects {
-afterEvaluate {
-if (plugins.hasPlugin("com.android.library")) {
-extensions.findByName("android")?.let { ext ->
-val androidExt = ext as LibraryExtension
-if (androidExt.namespace == null) {
-androidExt.namespace = "com.autofix.${project.name}"
-}
-}
-}
-}
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
-delete(rootProject.layout.buildDirectory)
+    delete(rootProject.layout.buildDirectory)
 }
